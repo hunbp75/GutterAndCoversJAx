a037bd89c55c0869fc7d941ae573d8744ac1a33d
hunbp75 <hunbp75@gmail.com>
2022-03-18 10:39:21 +0100

    add NavBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
+import Main from "./components/Main";
+import NavBar from "./components/NavBar";
+import ContactUs from "./pages/ContactUs";
+import Faqs from "./pages/Faqs";
+import Feedback from "./pages/Feedback";
+import Gallery from "./pages/Gallery";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
+const Layout = () => (
+  <div>
+    <NavBar />
+    <Main>
+      <Outlet />
+    </Main>
+  </div>
+);
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="Products" element={<Products />} />
+          <Route path="Gallery" element={<Gallery />} />
+          <Route path="ContactUs" element={<ContactUs />} />
+          <Route path="Feedback" element={<Feedback />} />
+          <Route path="Faqs" element={<Faqs />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
