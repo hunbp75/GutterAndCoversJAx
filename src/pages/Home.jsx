0fc33393dc2f72e3644f545faecf5c65219a7cdd
hunbp75 <hunbp75@gmail.com>
2022-03-18 11:12:08 +0100

    add text and style to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,20 @@
+import "./home.css";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div className="home-container">
+      <p className="text">
+        Gutters & Covers Llc. is a fully insured and licensed family owned
+        business with the highest standards of quality, we warrant all of the
+        materials and the have the highest warranty on Craftsmanship in
+        Jacksonville and surrounding areas. It is my sincere desire to give our
+        customers the best possible products and services available in the most
+        customer-friendly timeframe possible. I encourage you to give Gutters &
+        Covers Llc. the opportunity to serve you.
+      </p>
+      <p className="footer">Sincerely Peter Stampf - Owner and President</p>
+    </div>
+  );
 };
 
 export default Home;
