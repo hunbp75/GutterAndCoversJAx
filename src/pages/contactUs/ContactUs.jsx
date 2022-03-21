08da66f06093044d14f9d08800ffd6e676712c26
hunbp75 <hunbp75@gmail.com>
2022-03-21 15:16:06 +0100

    add contact ut text and style

--- src/pages/contactUs/ContactUs.jsx
+++ src/pages/contactUs/ContactUs.jsx
@@ -1,5 +1,26 @@
+import "./contactUs.css";
+
 const ContactUs = () => {
-  return <h1>Contact Us</h1>;
+  return (
+    <div className="contactus-container">
+      <h1>Gutters & Covers LLC.</h1>
+      <h2>9253 Arbolita way</h2>
+      <h2>Jacksonville, Florida, 32256</h2>
+      <div className="phone">
+        <span class="material-icons" id="call">
+          call
+        </span>
+        <h2>(904) 997-6685</h2>
+      </div>
+
+      <div className="email">
+        <span class="material-icons">email</span>
+        <a class="mailTo" href="mailto:contact@test.com">
+          gandcjax@gmail.com
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default ContactUs;
