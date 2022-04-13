5aa0c11b6434d73d0145962281c7aa522c90b864
hunbp75 <hunbp75@gmail.com>
2022-04-13 19:45:33 +0200

    work on map and modal

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,19 +1,17 @@
 import guttersImages from "./images/guttersImages.json";
-import protectionImages from "./images/protectionImages.json";
-import soffitAndFascia from "./images/soffitAndFascia.json";
-import aluminumRoof from "./images/aluminumRoof.json";
 import "./gallery.css";
 import { useEffect, useState } from "react";
-import Modal from "react-modal";
+import Modal from "./images/Modal";
 
 const Gallery = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedPicture, setSelectedPicture] = useState(null);
+  const [selectedPictureId, setSelectedPictureId] = useState();
   const [showButton, setShowButton] = useState(false);
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      if (window.pageYOffset > 500) {
+      if (window.pageYOffset > 200) {
         setShowButton(true);
       } else {
         setShowButton(false);
@@ -30,6 +28,7 @@ const Gallery = () => {
 
   const openModal = (item) => {
     setIsOpen(true);
+    setSelectedPictureId(item.id);
     setSelectedPicture(item.url);
   };
 
@@ -38,147 +37,88 @@ const Gallery = () => {
     setSelectedPicture(null);
   };
 
+  const guttersData = () => (
+    <div className="mapImgContainer">
+      {guttersImages.map((item) => {
+        if (item.title === "gutter") {
+          return (
+            <img
+              key={item.id}
+              className="image"
+              src={item.url}
+              alt="pics"
+              onClick={() => {
+                openModal(item);
+              }}
+            />
+          );
+        }
+      })}
+    </div>
+  );
+
+  const protectionData = () => (
+    <div className="mapImgContainer">
+      {guttersImages.map((item) => {
+        if (item.title === "protection") {
+          return (
+            <img
+              key={item.id}
+              className="image"
+              src={item.url}
+              alt="pics"
+              onClick={() => {
+                openModal(item);
+              }}
+            />
+          );
+        }
+      })}
+    </div>
+  );
+
+  const soffitData = () => (
+    <div className="mapImgContainer">
+      {guttersImages.map((item) => {
+        if (item.title === "soffit") {
+          return (
+            <img
+              key={item.id}
+              className="image"
+              src={item.url}
+              alt="pics"
+              onClick={() => {
+                openModal(item);
+              }}
+            />
+          );
+        }
+      })}
+    </div>
+  );
+
+  const aluData = () => (
+    <div className="mapImgContainer">
+      {guttersImages.map((item, index) => {
+        if (item.title === "alu") {
+          return (
+            <img
+              key={index}
+              className="image"
+              src={item.url}
+              alt="pics"
+              onClick={() => {
+                openModal(item);
+              }}
+            />
+          );
+        }
+      })}
+    </div>
+  );
+
   return (
     <div className="kkl">
-      <h1 className="gallery-title">Gutter</h1>
-      <div className="image-container">
-        {guttersImages.map((item) => {
-          return (
-            <div key={item.id}>
-              <img
-                className="image"
-                src={item.url}
-                alt="pics"
-                onClick={() => openModal(item)}
-              />
-              <Modal
-                className="modal"
-                isOpen={isOpen}
-                onRequestClose={closeModal}
-                ariaHideApp={false}
-              >
-                <div className="modal-container">
-                  <div className="modal-pictures">
-                    <p>X</p>
-                    <img
-                      id="modal-img"
-                      src={selectedPicture}
-                      alt="pics"
-                      onClick={closeModal}
-                    />
-                  </div>
-                </div>
-              </Modal>
-            </div>
-          );
-        })}
-      </div>
-
-      <h1 className="gallery-title">Gutter Protection</h1>
-      <div className="image-container">
-        {protectionImages.map((item) => {
-          return (
-            <div key={item.id}>
-              <img
-                className="image"
-                src={item.url}
-                alt="pics"
-                onClick={() => openModal(item)}
-              />
-              <div className="modal" id="modal">
-                <Modal
-                  className="modal"
-                  id="modal"
-                  isOpen={isOpen}
-                  onRequestClose={closeModal}
-                  ariaHideApp={false}
-                >
-                  <div className="modal-container">
-                    <div className="modal-pictures">
-                      <img
-                        id="modal-img"
-                        src={selectedPicture}
-                        alt="pics"
-                        onClick={closeModal}
-                      />
-                    </div>
-                  </div>
-                </Modal>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <h1 className="gallery-title">Soffit and Fascia</h1>
-      <div className="image-container">
-        {soffitAndFascia.map((item) => {
-          return (
-            <div key={item.id}>
-              <img
-                className="image"
-                src={item.url}
-                alt="pics"
-                onClick={() => openModal(item)}
-              />
-              <div className="modal" id="modal">
-                <Modal
-                  className="modal"
-                  id="modal"
-                  isOpen={isOpen}
-                  onRequestClose={closeModal}
-                  ariaHideApp={false}
-                >
-                  <div className="modal-container">
-                    <div className="modal-pictures">
-                      <img
-                        id="modal-img"
-                        src={selectedPicture}
-                        alt="pics"
-                        onClick={closeModal}
-                      />
-                    </div>
-                  </div>
-                </Modal>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <h1 className="gallery-title">Aluminum Roof</h1>
-      <div className="image-container">
-        {aluminumRoof.map((item) => {
-          return (
-            <div key={item.id}>
-              <img
-                className="image"
-                src={item.url}
-                alt="pics"
-                onClick={() => openModal(item)}
-              />
-              <div className="modal" id="modal">
-                <Modal
-                  className="modal"
-                  id="modal"
-                  isOpen={isOpen}
-                  onRequestClose={closeModal}
-                  ariaHideApp={false}
-                >
-                  <div className="modal-container">
-                    <div className="modal-pictures">
-                      <img
-                        id="modal-img"
-                        src={selectedPicture}
-                        alt="pics"
-                        onClick={closeModal}
-                      />
-                    </div>
-                  </div>
-                </Modal>
-              </div>
-            </div>
-          );
-        })}
-      </div>
       <div className="back-button">
         {showButton && (
           <button onClick={scrollToTop} className="back-to-top">
@@ -186,6 +126,22 @@ const Gallery = () => {
           </button>
         )}
       </div>
+      <h1 className="gallery-title">Gutter</h1>
+      {guttersData()}
+      <h1 className="gallery-title">Gutter Protection</h1>
+      {protectionData()}
+      <h1 className="gallery-title">Soffit And Fascia</h1>
+      {soffitData()}
+      <h1 className="gallery-title">Aluminum Roof</h1>
+      {aluData()}
+      <Modal
+        selectedPicture={selectedPicture}
+        selectedPictureId={selectedPictureId}
+        setSelectedPicture={setSelectedPicture}
+        setSelectedPictureId={setSelectedPictureId}
+        isOpen={isOpen}
+        closeModal={closeModal}
+      />
     </div>
   );
 };
